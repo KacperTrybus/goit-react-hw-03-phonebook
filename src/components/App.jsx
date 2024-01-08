3ac0385d4f6f76ea6c0724654418c2114f5c7fe3
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2024-01-08 20:11:08 +0100

    LocalStorage added
    Contacts save in local storage now

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,19 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   contactExists = newContact => {
     const { contacts } = this.state;
     return contacts.some(
